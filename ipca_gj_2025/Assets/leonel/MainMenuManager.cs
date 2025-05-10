4c73b2495944239a227a62c4d95c81fb67a78f26
Vulpix98 <leonelqo@hotmail.com>
2025-05-10 01:14:47 +0100

    change scene, only settings, still to do

---
+++ ipca_gj_2025/Assets/leonel/MainMenuManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    // public void OpenSettings()
+    // {
+    //     SceneManager.LoadScene("Settings");
+    // }
+
+}
